dac640bb15173108b0f483fc866eb05ad22c47a0
JKZhang <1849104785@qq.com>
2023-09-19 14:24:53 +0800

    init

---
+++ java/src/test001.java
@@ -0,0 +1,10 @@
+/**
+ * @author ZhangJiekai
+ * @description
+ * @date 2023/09/19
+ */
+public class test001 {
+    public static void main(String[] args){
+        System.out.println("hello!");
+    }
+}
